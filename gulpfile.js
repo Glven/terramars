69cb07f9d05d4ebd103076d9a06fda4df69b14e8
Glven <t-oganesyan00@yandex.ru>
2022-04-21 15:28:38 +0300

    installing browser-sync and gulp-sass compiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
+var {src, dest, watch} = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+var browserSync = require('browser-sync').create();
 
-gulp.task('hello', function(done){
-    console.log('Соси, мир!');
-    done();
-});
-
-gulp.task('browser-sync', function() {
+function bs() {
+    serveSass();
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: "src"
         }
     });
-    gulp.watch("./*.html").on('change', browserSync.reload);
-});
+    watch("./src/sass/**/*.sass").on("change", serveSass);
+    watch("./src/*.html").on("change", browserSync.reload);
+    watch("./js/*.js").on("change", browserSync.reload);
+};
+
+function serveSass() {
+    return src("./src/sass/**/*.sass")
+        .pipe(sass())
+        .pipe(dest("./src/css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
