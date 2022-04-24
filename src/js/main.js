c598781d3634c21f6192483396d277c639c95ed4
Glven <t-oganesyan00@yandex.ru>
2022-04-24 23:08:40 +0300

    fixed header

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,33 @@
 $(document).ready(function () {
+    var headerSearch = $('.header-nav__form');
+    var searchIcon = $('.header-nav__searchIcon');
 
-    var containers = $('.header, .footer, .section');
-    var headerSearch = $('.header-search__form');
-
-    $('.header-search__icon').click(function(e){
-        e.preventDefault();
-        headerSearch.addClass('header-search__form_active');
-        $(this).css({'opacity' : '0'});
-    });
-    $('body').keydown(function(e){
-        if(e.which == 27){
-            headerSearch.removeClass('header-search__form_active');
-            $('.header-search__icon').css({'opacity':'1'});
-        }
-    });
-    $(document).mouseup(function(e){
-        e.preventDefault();
-        if(headerSearch.has(e.target).length==0){
-            headerSearch.removeClass('header-search__form_active');
-            $('.header-search__icon').css({'opacity':'1'});
-        }
-    });
     $(window).scroll(function(e){
         e.preventDefault();
-        if($(window).scrollTop() > $('.section').height() / 2){
+        if($(window).scrollTop() > $('section').height() / 3){
             $('.header-fixed').addClass('header-fixed_active');
         }
         else{
             $('.header-fixed').removeClass('header-fixed_active');
         }
-      });
+    });
+    searchIcon.click(function(e){
+        e.preventDefault();
+        headerSearch.addClass('header-nav__form_active');
+        $(this).addClass('header-nav__search_inactive');
+    });
+    $('body').keydown(function(e){
+        if (e.which == 27){
+            headerSearch.removeClass('header-nav__form_active');
+            searchIcon.removeClass('header-nav__search_inactive');
+        }
+    });
+    $(document).mouseup(function(e){
+        e.preventDefault();
+        if(headerSearch.has(e.target).length == 0){
+            headerSearch.removeClass('header-nav__form_active');
+            $('.header-nav__searchIcon').css({'display':'flex'});
+            searchIcon.removeClass('header-nav__search_inactive');
+        }
+    });
 });
