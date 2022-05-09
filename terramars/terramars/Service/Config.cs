ac62c141176786e913506a174d3c59717b7f1a8d
Glven <t-oganesyan00@yandex.ru>
2022-05-09 22:47:16 +0300

    test

---
+++ terramars/terramars/Service/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terramars.Service
+{
+    public class Config
+    {
+        public static string CompanyName { get; set; }
+        public static string CompanyPhone { get; set; }
+        public static string CompanyEmail { get; set; }
+    }
+}
